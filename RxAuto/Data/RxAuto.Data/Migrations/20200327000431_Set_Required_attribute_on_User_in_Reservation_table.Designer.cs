0cbda2f9db6b2e323f42de13382ee7acc9af16e8
radrex <radostinv.stoychev@gmail.com>
2020-03-27 02:17:55 +0200

    Set [Required] attribute on User in Reservation table migration

--- RxAuto/Data/RxAuto.Data/Migrations/20200327000431_Set_Required_attribute_on_User_in_Reservation_table.Designer.cs
+++ RxAuto/Data/RxAuto.Data/Migrations/20200327000431_Set_Required_attribute_on_User_in_Reservation_table.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RxAuto.Data;
 
 namespace RxAuto.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200327000431_Set_Required_attribute_on_User_in_Reservation_table")]
+    partial class Set_Required_attribute_on_User_in_Reservation_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RxAuto/Data/RxAuto.Data/Migrations/20200327000431_Set_Required_attribute_on_User_in_Reservation_table.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RxAuto.Data.Migrations
+{
+    public partial class Set_Required_attribute_on_User_in_Reservation_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Reservations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Reservations",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
